5bdb7393531f59afd5bce8e9ef2e829db5dcfcf4
Jason G <jasong@MacBook-Air-2.local>
2018-04-02 17:10:46 -0500

    initial commit

---
+++ checkout.php
@@ -0,0 +1,30 @@
+<?php
+//get post-data where my brain doesn't hurt
+$name = $_POST['who'];
+$items[] = $_POST['checkout'];
+$items = $items[0];
+$number_of_items = count($items);
+
+//open db connection
+include '/etc/phpstuff/salesselector.php';
+$db = new mysqli('127.0.0.1', $sqlUser, $sqlPass,
+'thrashca_salesselector');
+
+//find the numerical id of the sales person
+$query = "SELECT `id` FROM people WHERE name='".$name."';";
+$result = $db->query($query);
+$line = $result->fetch_array();
+$id = $line[0];
+
+//set items unavailable and taken by the person
+$i = $number_of_items;
+while ($i > 0) {
+  $query = "UPDATE equipment SET checked_out=1, who_has=";
+  $query = $query.$id." WHERE tag_num=".$items[($i-1)].";";
+  $db->query($query);
+  $i--;
+}
+
+
+?>
+<a href="http://192.168.1.126/salesselector/equipment-checkout.php">Go Back</a>
---
+++ equipment-checkout.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="main.css?v=04012018b">
+	</head>
+	<body>
+		Who are you?
+		<form action="checkout.php" method="POST">
+			<?php
+
+			include '/etc/phpstuff/salesselector.php';
+
+			$db = new mysqli('127.0.0.1', $sqlUser, $sqlPass,
+'thrashca_salesselector');
+			$query = "SELECT * FROM people;";
+			$result = $db->query($query);
+
+			while (2 < 5) {
+				$line = $result->fetch_array();
+				if ($line == NULL) {break;}
+				echo '<p><input type="radio" name="who"
+value="'.$line['name'].'">';
+				echo ' '.$line['name']."</p>";
+			}
+			?>
+		<p>And what are you taking with you?</p>
+		<table style="width:80%; border: 1px solid black;">
+			<tr>
+				<th>Tag Number</th>
+				<th>Description</th>
+				<th>Checked Out By</th>
+				<th>Check Out</th>
+			</tr>
+			<?php
+				$query = "SELECT * FROM equipment";
+				$query = $query." LEFT OUTER JOIN people ON equipment.who_has = people.id";
+				$query = $query." ORDER BY `description`;";
+
+				$result = $db->query($query);
+				while (2 < 5) {
+					$line = $result->fetch_array();
+					if ($line == NULL) {break;}
+					$tag_num = $line['tag_num'];
+					$description = $line['description'];
+					$checked_out_by = $line['name'];
+
+					/*if the equipment is already checked out, say by who.
+					otherwise, offer a check box to check it out */
+					if ($checked_out_by === NULL) {
+						echo "<tr><td>".$tag_num."</td><td>".$description;
+						echo "</td><td></td><td>";
+						echo '<input type="checkbox" name="checkout[]" value="'.$tag_num.'"></td></tr>';
+					}
+					else {
+						echo '<tr><td>'.$tag_num.'</td><td>'.$description.'</td>';
+						echo '<td>'.$checked_out_by.'</td><td></td></tr>';
+					}
+				}
+
+				$db->close();
+			?>
+		</table>
+		<input type="submit" value="Submit">
+		</form>
+	</body>
+</html>
